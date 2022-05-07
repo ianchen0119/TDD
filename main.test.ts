a73a71fba7dafc01a77019487e8e780c85604b71
Ian Chen <ychen.desl@gmail.com>
2022-05-07 14:54:06 +0800

    refactor main.test.ts

--- main.test.ts
+++ main.test.ts
@@ -1,20 +1,23 @@
 import porterKata from "./main";
 import { book } from "./main";
 
-test('Buy a book', () => {
-    let instant = new porterKata();
-    let hp1 :book = {name: "hp1", price: 100}
-    instant.pushItem(hp1);
-    let price: number = instant.cauculate();
-    expect(price).toBe(100);
-});
-
-test('Buy 2 books', () => {
-    let instant = new porterKata();
+(() => {
     let hp1 :book = {name: "hp1", price: 100}
     let hp2 :book = {name: "hp2", price: 100}
-    instant.pushItem(hp1);
-    instant.pushItem(hp2);
-    let price: number = instant.cauculate();
-    expect(price).toBe(190);
-});
+    let price: number = 0;
+
+    test('Buy a book', () => {
+        let instant = new porterKata();
+        instant.pushItem(hp1);
+        price = instant.cauculate();
+        expect(price).toBe(100);
+    });
+    
+    test('Buy 2 books', () => {
+        let instant = new porterKata();
+        instant.pushItem(hp1);
+        instant.pushItem(hp2);
+        price = instant.cauculate();
+        expect(price).toBe(190);
+    });
+})();
